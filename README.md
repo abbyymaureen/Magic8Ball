a62cf0d3df230f06507eee7fbac7e43d9c3b9267
Abby Brown <amb0330@icloud.com>
2024-03-06 21:58:21 -0700

    Finish docs

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# Magic8Ball
+# Magic 8 Ball
+
+Welcome to our Magic 8 Ball game! Here, you can enter any question you
+desire and have your fortune told to you. Below are steps for success
+on how to install and use our groundbreaking fortune teller.
+
+## Running Through an IDE
+1. Download the IDE of your choice (we suggest GoLand).
+```
+# GoLand (free with college account)
+MacOS: https://www.jetbrains.com/go/download/#section=mac
+Linux: https://www.jetbrains.com/go/download/#section=linux
+Windows: https://www.jetbrains.com/go/download/#section=windows
+```
+
+2. Clone our GitHub repository.
+```
+https://github.com/abbyymaureen/Magic8Ball.git
+```
+
+3. Open and Run
+Locate and open the GitHub repository on your computer and run it through the IDE
+by clicking the 'play' arrow button. Enjoy our application!
+
+## Running Through the Terminal
+1. Download Go for your computer
+```
+https://go.dev/dl/
+```
+
+2. Clone our GitHub repository.
+```
+https://github.com/abbyymaureen/Magic8Ball.git
+```
+
+3. Open the terminal and navigate to the repository.
+```
+Example: cd/downloads/Magic8Ball
+```
+
+4. Once you are in the proper folder, run the application:
+```
+go run main.go
+```
+
+And you are all set! Enjoy our application and have fun predicting your
+own future!
--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+// @author abbybrown
+// @date 03/06/2024
+// @file main.go
+// 		This file lets the user get their fortune by taking in the user's input and returning their
+//		fortune from a list of randomly generated responses.
+
+// make sure to put import statements in ()
 import (
 	"bufio"
 	"fmt"
@@ -9,23 +16,37 @@ import (
 	"time"
 )
 
+/*
+*
+@param min : int : the minimum index to select from
+@param max : int : the maximum index to select from
+@return random number between min and max (but can only take one argument, so it's kind of weird
+*/
 func randomInt(min, max int) int {
 	return rand.Intn(max-min+1) + min
 }
 
+/*
+*
+Lets the user ask a question as many times as they want and will give a random answer back
+*/
 func magicEightBall() {
+	// create all the messages possible
 	messages := []string{"It is certain", "Very doubtful", "Ask again later",
 		"Without a doubt", "As I see it, yes", "Don't count on it", "Cannot predict now",
 		"Outlook good", "Most likely", "Yes", "My source says no", "Reply hazy, try again",
 		"It is decidedly so", "Better not tell you now", "Outlook not so good", "My reply is no",
 		"You may rely on it", "Definitely yes", "Signs point to yes"}
 
+	// apparently while loops don't exist, so do a for loop that checks for 'q'
 	for {
 		randNum := randomInt(0, len(messages))
 
+		// print the menu
 		fmt.Println("* * * Magic Eight Ball of Mystery * * *")
 		fmt.Printf("Please ask your question (enter 'q' to quit): ")
 
+		// see if there is an error and return that error, then quit
 		reader := bufio.NewReader(os.Stdin)
 		answer, err := reader.ReadString('\n')
 		if err != nil {
@@ -33,24 +54,27 @@ func magicEightBall() {
 			return
 		}
 
-		// Remove newline character from the input
+		// split apart the user's string answer
 		answer = strings.TrimSpace(answer)
 
+		// if the user entered 'q,' then quit
 		if answer == "q" {
 			fmt.Println("Thank you for finding your fortune!")
 			return
 		}
 
+		// do a fun time.Sleep thing to build suspense
 		fmt.Print("... ")
-		time.Sleep(500 * time.Millisecond)
+		time.Sleep(600 * time.Millisecond)
 		fmt.Print("... ")
-		time.Sleep(500 * time.Millisecond)
+		time.Sleep(600 * time.Millisecond)
 		fmt.Print("...\n")
-		time.Sleep(500 * time.Millisecond)
-		fmt.Println(messages[randNum], "\n")
+		time.Sleep(200 * time.Millisecond)
+		fmt.Println(messages[randNum], "\n") // I want that new line for style purposes
 	}
 }
 
 func main() {
+	// call our function!
 	magicEightBall()
 }
